9fe7000a639988d10e1a369edba1db8fd6567d16
unknown <maks-kandikov@yandex.ru>
2024-09-23 22:25:02 +0300

    color added

--- src/checker.py
+++ src/checker.py
@@ -1,17 +1,17 @@
 import logging
 import concurrent.futures
+from colorama import Fore
 from src.smtp_verifier import check_email
 from src.file_handler import read_emails, write_results
+from src.utils import colored
 
 
 def check_single_email(email, delay):
     email, status = check_email(email, delay, [])
     if email and status == "Valid":
-        logging.info(f'{email} is valid.')
+        logging.info(f'{colored(email, Fore.GREEN)} is valid.')
     elif email and status == "Suspicious":
-        logging.info(f'{email} is suspicious.')
-    else:
-        logging.info(f'{email} is invalid: {status}')
+        logging.info(f'{colored(email, Fore.YELLOW)} is suspicious.')
 
 
 def process_emails(input_file, output_file, suspicious_file, delay, threads):
@@ -28,14 +28,13 @@ def process_emails(input_file, output_file, suspicious_file, delay, threads):
             try:
                 result, status = future.result()
                 if result and status == "Valid":
-                    logging.info(f'{result} is valid.')
+                    logging.info(f'{colored(result, Fore.GREEN)} is valid.')
                     valid_emails.append(result)
                 elif result and status == "Suspicious":
-                    logging.info(f'{result} is Suspicious.')
+                    logging.info(f'{colored(result, Fore.YELLOW)} is suspicious.')
                     suspicious_emails.append(result)
             except Exception as exc:
-                logging.error(f'{email} generated an exception: {exc}')
+                logging.error(f'{colored(email, Fore.RED)} generated an exception: {exc}')
 
     write_results(valid_emails, suspicious_emails, output_file, suspicious_file)
-    logging.info(f'Finished processing {len(emails)} emails.')
-
+    logging.info(f'{colored(f"Finished processing {len(emails)} emails.", Fore.BLUE)}')
