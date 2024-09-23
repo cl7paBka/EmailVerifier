25d5ca49934fc97aae22c694848cc526bca5c2d7
unknown <maks-kandikov@yandex.ru>
2024-09-23 20:45:49 +0300

    Rename

--- EV.py
+++ EV.py
@@ -5,14 +5,15 @@ from src.checker import process_emails, check_single_email
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 def print_logo():
-    logo = f"""
+    logo = r"""
      /\_/\                   /\_/\ 
     ( o.o )                 ( o.o )
      > ^ <                   > ^ <
     ###############################     
-    #-#                         #-#     
-    #  -#                     #-  #     
+    #-#      EmailVerifier      #-#     
+    #  -#     by cl7paBka     #-  #     
     #    -#                 #-    #     
     #      .#             #.      #     
     #        .#         #.        #     
@@ -29,9 +30,11 @@ def print_logo():
 
 def parse_args():
     parser = argparse.ArgumentParser(description=f'{print_logo()}Email Verifier CLI Tool')
-    parser.add_argument('input', nargs='?', default='input.txt', help='Path to the input file with email addresses')
-    parser.add_argument('-o', '--output', default='validated_addresses.txt', help='Path to the output file for valid emails')
-    parser.add_argument('-s', '--suspicious', default='suspicious_addresses.txt',
+    parser.add_argument('input', nargs='?', default=r'.\src\files\input.txt',
+                        help='Path to the input file with email addresses')
+    parser.add_argument('-o', '--output', default=r'.\src\files\validated_addresses.txt ',
+                        help='Path to the output file for valid emails')
+    parser.add_argument('-s', '--suspicious', default=r'.\src\files\suspicious_addresses.txt',
                         help='Path to the output file for B2C adresses\n'
                              '(this tool does not work very well against the big boys who have their own procedures and spam rules e.g.hotmail and yahoo.)')
     parser.add_argument('-d', '--delay', type=float, default=1.0, help='Delay between SMTP requests (in seconds)')
@@ -43,18 +46,23 @@ def parse_args():
 
 
 def run():
-    args = parse_args()
+    try:
+        args = parse_args()
 
-    # Проверка одиночного email
-    if args.email:
-        check_single_email(args.email, args.delay)
-    else:
-        # Проверка email из файла
-        if not os.path.exists(args.input):
-            logging.error(f'Input file {args.input} does not exist.')
-            exit(1)
+        # Проверка одиночного email
+        if args.email:
+            check_single_email(args.email, args.delay)
+        else:
+            # Проверка email из файла
+            if not os.path.exists(args.input):
+                logging.error(f'Input file {args.input} does not exist.')
+                exit(1)
 
-        process_emails(args.input, args.output, args.suspicious, args.delay, args.threads)
+            process_emails(args.input, args.output, args.suspicious, args.delay, args.threads)
+
+    except KeyboardInterrupt:
+        logging.info("Program interrupted by user (Ctrl+C). Exiting...")
+        exit(0)
 
 
 if __name__ == '__main__':
