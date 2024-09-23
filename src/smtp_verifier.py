586391bab6b4df474c715f7bf8d7e3e9e7d322e8
unknown <maks-kandikov@yandex.ru>
2024-09-23 22:25:09 +0300

    color added

--- src/smtp_verifier.py
+++ src/smtp_verifier.py
@@ -3,7 +3,9 @@ import smtplib
 import time
 import logging
 import dns.resolver
+from colorama import Fore
 from src.config import B2C_PROVIDERS, from_address
+from src.utils import colored
 
 regex = r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$'
 domain_mx_records = {}
@@ -11,7 +13,7 @@ domain_mx_records = {}
 
 def check_email(email, delay, suspicious_domains):
     if not re.match(regex, email):
-        logging.error(f'Invalid syntax: {email}')
+        logging.error(f'Invalid syntax: {colored(email, Fore.RED)}')
         return None, "Invalid syntax"
 
     domain = email.split('@')[1]
@@ -40,10 +42,10 @@ def check_email(email, delay, suspicious_domains):
         else:
             return None, f"Failed SMTP check: {code} {message}"
     except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN) as e:
-        logging.error(f'DNS error for {email}: {e}')
+        logging.error(f'{colored("DNS", Fore.RED)} error for {colored(email, Fore.RED)}: {e}')
         return None, "DNS error"
     except Exception as e:
-        logging.error(f'SMTP error for {email}: {e}')
+        logging.error(f'{colored("SMTP", Fore.RED)} error for {colored(email, Fore.RED)}: {e}')
         return None, "SMTP error"
     finally:
         time.sleep(delay)
